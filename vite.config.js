1e19a7300d7c908fbc1fbdbf86415bb7ee99ad9a
HireIQ <squaredummy01@gmail.com>
2026-03-12 23:37:22 +0530

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:3000'
-    }
+  root: '.',
+  build: {
+    outDir: 'dist'
   }
 })
